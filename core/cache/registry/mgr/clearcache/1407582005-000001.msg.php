697ebc893dd2543c6213b7ae8d3d3aa1a339e9dd
Thomas Stradling <protolich@gmail.com>
2014-08-09 12:44:59 +0100

    Deleted cache

--- core/cache/registry/mgr/clearcache/1407582005-000001.msg.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'timestamp' => '2014-08-09 12:00:05',
-  'level' => 'INFO',
-  'msg' => '-> <strong>0</strong> documents were published.',
-  'def' => '',
-  'file' => '/connectors/index.php',
-  'line' => '',
-);
